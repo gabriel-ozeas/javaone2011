90f1ee9c21713fb4bf7b15e2229c15ebc189dab3
Gabriel Ozeas <gabriel.ozeas1@gmail.com>
2011-11-29 18:27:50 -0200

    Adding MovieBean

---
+++ javaone2011-jsf12/src/main/java/br/com/fourlinux/videostore/MovieStatisticBean.java
@@ -0,0 +1,36 @@
+package br.com.fourlinux.videostore;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+
+public class MovieStatisticBean {
+	private Movie movie;
+	private float rate;
+	
+	private MovieBean movieBean;
+	
+	public void rateMovie(ActionEvent event) {
+		if (movie.getStatistics() != null) {
+			movie.setStatistics(new MovieStatistics());
+		}
+		MovieStatistics statistics = movie.getStatistics();
+		
+		float oldTotalRate = statistics.getRate() * statistics.getTotalVotes();
+		float newTotalRate = oldTotalRate + rate;
+		
+		statistics.setTotalVotes(statistics.getTotalVotes() + 1);
+		float newRate = newTotalRate / statistics.getTotalVotes();
+		
+		statistics.setRate(newRate);
+		
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Avaliação registrada com sucesso!", null);
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+	
+	
+	public void voteAsFavorite(ActionEvent event) {
+		MovieStatistics statistics = movie.getStatistics();
+	}
+}
